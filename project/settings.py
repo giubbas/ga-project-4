9d7ece043a6f2cc719e139b6b1ac934d2e1b8bfd
riccardo <riccardogiubiloweb@gmail.com>
2022-06-28 16:06:02 +0200

    commitment

--- project/settings.py
+++ project/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
         'DIRS': [os.path.join(BASE_DIR, 'client')
         ]  #Look, we have added the root folder of frontend here
         ,
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
