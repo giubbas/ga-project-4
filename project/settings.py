33fb48ca975e57025ecb16525d9d0bad925d1605
riccardo <riccardogiubiloweb@gmail.com>
2022-06-28 15:04:02 +0200

    fix, try to deploy again

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-django_on_heroku.settings(locals())
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -143,10 +142,8 @@ REST_FRAMEWORK = {
     ]
 }
 
-ROOT_URLCONF = 'project.urls' #check if you have this already, if not add it in
-
-STATIC_URL = '/static/' # same with this
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'client', "build", "static"),
 )
+
+django_on_heroku.settings(locals())
