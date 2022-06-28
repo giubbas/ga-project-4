f6891da2c71d31442363dc3b0bb8838abfaff34d
riccardo <riccardogiubiloweb@gmail.com>
2022-06-28 16:39:11 +0200

    try to deploy

--- project/settings.py
+++ project/settings.py
@@ -1,5 +1,5 @@
-import os
 import django_on_heroku
+import os
 """
 Django settings for project project.
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
