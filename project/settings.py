e7fef313250a958f6858efe0ccb0f362f9e3b402
riccardo <riccardogiubiloweb@gmail.com>
2022-07-08 17:16:20 +0200

    try to fix

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import environ
 env = environ.Env()
 environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
