68016392db9af0687801841105772a1a7f6b47bb
riccardo <riccardogiubiloweb@gmail.com>
2022-07-08 17:48:26 +0200

    try to deploy

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path 
-from .views import index # <-- also new
+# from .views import index # <-- also new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
