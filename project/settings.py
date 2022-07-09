b09af950a7df8b4891388d8d794053dfba1db6e3
riccardo <riccardogiubiloweb@gmail.com>
2022-07-09 10:31:13 +0200

    seeds completed

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -142,7 +141,7 @@ ROOT_URLCONF = 'project.urls'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'client', "build", "static").replace('\\','/'),
+    os.path.join(BASE_DIR, 'client', "build", "static"),
 )
 
 django_on_heroku.settings(locals())
