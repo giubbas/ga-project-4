fffd30a0e069c9b0ee1d6d32adfc427f3eb0b96e
riccardo <riccardogiubiloweb@gmail.com>
2022-07-12 19:39:25 +0200

    secret key environ

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x-#!&b=n7rb&%e2$kmkt^cn$qh7*zd0=y3obj_)dq0!+(=(!76'
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
