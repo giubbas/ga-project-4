d9d3538c0762b51aad8e996c38ecc499baad11c2
riccardo <riccardogiubiloweb@gmail.com>
2022-07-12 19:05:35 +0200

    order changed installed apps

--- project/settings.py
+++ project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'jwt_auth',
     'recipes_app',
+    'jwt_auth',
     'comments',
 ]
 
