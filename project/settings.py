750a5ea5f80ac9136fe39068ac2b5169dffbd8be
riccardo <riccardogiubiloweb@gmail.com>
2022-07-22 12:37:32 +0200

    changed environ

--- project/settings.py
+++ project/settings.py
@@ -12,8 +12,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
+
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
