329ad2f58f84c3eb636c0f40374314a8bfd03844
riccardo <riccardogiubiloweb@gmail.com>
2022-07-28 18:49:08 +0200

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'client')
-                 ]  #Look, we have added the root folder of frontend here
+                ]  #Look, we have added the root folder of frontend here
         ,
         'APP_DIRS': True,
         'OPTIONS': {
